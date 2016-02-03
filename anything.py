435cb39f11769e2aa08f8c06ee7571781307656d
mrkezii <david.kezi@yahoo.com>
2016-02-03 15:11:09 +0300

    creating a main script

---
+++ anything.py
@@ -0,0 +1,7 @@
+def main():
+	print("hello ist me")
+	egg()
+	
+def egg():
+	print("eggs")
+if __name__ == "__main__":main()
---
+++ david.py
@@ -0,0 +1,12 @@
+my_name = 'david Kezi'
+my_age = 19
+my_height = 6.7
+my_weight = 120
+my_eyes = "brown"
+my_hair = 'black'
+
+print "my name is %s" % my_name
+print "my age is %d" % my_age
+print "my eyes are %s" % my_eyes
+
+print "if i add %d, %d and %d... i'll get %d"  %(my_age, my_height, my_weight ,my_age + my_height + my_weight)
