42730ad09bbe004b34727ac8ffe080712631bd22
mrkezii <david.kezi@yahoo.com>
2016-02-08 13:15:25 +0300

    learnt how to use map and filter but got stuck when trying to use reduce

--- lambda_functions.py
+++ lambda_functions.py
@@ -1,4 +1,15 @@
 def main():
-
+	a = [1,3,54,32,12,34,5,9]
+	
+	y = list(map(lambda x: x+2 , a))
+	print (y)
+	
+	z = list(filter(lambda x: x%2 , a))
+	print (z)
+	
+	# w = functools.reduce( p if (p > b ) else b , [1,3,54,32,12,34,5,9])
+# 	print (w)
+# i couldnt get how i could use reduce in python 3 ,
+# apparently they removed it and theres very little documentation.
 
 if __name__ == "__main__":main()
