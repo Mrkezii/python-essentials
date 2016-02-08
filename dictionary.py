e43a06cff2ed6a18a863cbb7b7c05aebdc7cdc50
mrkezii <david.kezi@yahoo.com>
2016-02-08 05:03:53 +0300

    dictionary used

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,11 @@
 def main():
-
-
-if__name__=="__main__": main()
+	d = dict(
+		one = 1, two = 2 , three = 3, four = 4, five = 5, six = 6
+	
+	
+	)
+	
+	for k in d:
+		print(k, d[k])
+		
+if __name__== "__main__": main()
