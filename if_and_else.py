0d3d72612e718462bb55bb2dd9f0aae9bce9f4f8
mrkezii <david.kezi@yahoo.com>
2016-02-10 16:02:25 +0300

    switch and if_else

---
+++ if_and_else.py
@@ -0,0 +1,8 @@
+def main():
+	a,b= 0, 1
+	
+	if True:
+		print('dave')
+	else:
+		print('not')
+if __name__ == "__main__": main()
---
+++ switch.py
@@ -0,0 +1,12 @@
+def main():
+	choice = dict(
+		one = 'first',
+		two = 'second',
+		three = 'third',
+		four = 'four'
+	)
+
+	v = 'three'
+	print(choice[v])
+
+if __name__ == "__main__":main()
